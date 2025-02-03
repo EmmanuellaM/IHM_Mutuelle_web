4282d57869514db0892316372118fea6e8abaff0
leg234-png <wandjilegentia@gmail.com>
2025-02-03 08:31:20 +0100

    Update database configuration

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=mutuelle',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=sql.freedb.tech;dbname=freedb_mutuelle',
+    'username' => 'freedb_wandji',
+    'password' => 'AfC9zKpNmX2P%T$',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
