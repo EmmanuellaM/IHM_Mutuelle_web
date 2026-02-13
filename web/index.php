a0b01a0dc517a3c389bb0123935eeec258b371fc
EmmanuellaM <emmanuellambiake127@gmail.com>
2026-02-13 08:24:03 +0100

    Cleanup: Remove diagnostic scripts and restore project state

--- web/index.php
+++ web/index.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// Trigger deployment
+
 
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
