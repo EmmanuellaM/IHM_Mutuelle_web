eac81c7ea389e6590df74ba48665c5e664d34564
EmmanuellaM <emmanuellambiake127@gmail.com>
2026-02-13 07:29:27 +0100

    DEBUG: Force controller output to test blank page issue

--- controllers/AdministratorController.php
+++ controllers/AdministratorController.php
@@ -112,7 +112,8 @@ class AdministratorController extends Controller
         $model->interest = SettingManager::getInterest();
         $model->inscription_amount = SettingManager::getInscription();
         $model->social_crown_amount = SettingManager::getSocialCrown();
-        return $this->render('home', compact('session', 'model', 'idModel'));
+        // return $this->render('home', compact('session', 'model', 'idModel'));
+        return "DEBUG: Controller is working. User ID: " . $this->user->id . ", Admin ID: " . $this->administrator->id;
     }
     // Nouvelle Session (ancien)
     // public function actionNouvelleSession()
